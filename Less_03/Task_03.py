95ca835c8815ddff1197d1b783ba0ee17ffb9f84
mechanic3000 <alx.illarionov@gmail.com>
2021-02-05 14:33:00 +0300

    Lesson 03 / 03 some add

--- Less_03/Task_03.py
+++ Less_03/Task_03.py
@@ -11,14 +11,19 @@ MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
-min_item = [0, MAX_ITEM]
-max_item = [0, 0]
 
 for i, item in enumerate(array):
-    if item < min_item[1]:
-        min_item[0], min_item[1] = i, item
-    elif item > max_item[1]:
-        max_item[0], max_item[1] = i, item
+    try:
+        if item < min_item[1]:
+            min_item[0], min_item[1] = i, item
+    except NameError:
+        min_item = [i, item]
+
+    try:
+        if item > max_item[1]:
+            max_item[0], max_item[1] = i, item
+    except NameError:
+        max_item = [i, item]
 
 array[min_item[0]], array[max_item[0]] = max_item[1], min_item[1]
 print(array)
