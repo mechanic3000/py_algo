b0e0f9e3036f604639238b109e8818e6f43b04d8
mechanic3000 <alx.illarionov@gmail.com>
2021-02-18 21:34:48 +0300

    Lesson 06 / so-so

--- Less_03/Task_03.py
+++ Less_03/Task_03.py
@@ -4,6 +4,7 @@
 """
 
 import random
+import sys
 
 SIZE = 10
 MIN_ITEM = 0
@@ -11,6 +12,9 @@ MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
+
+def get_vol(i)->int:
+    return sys.getsizeof(i)
 
 for i, item in enumerate(array):
     try:
@@ -25,5 +29,11 @@ for i, item in enumerate(array):
     except NameError:
         max_item = [i, item]
 
+sum = int()
+sum += get_vol(min_item)
+sum += get_vol(max_item)
+sum += get_vol(array)
+
 array[min_item[0]], array[max_item[0]] = max_item[1], min_item[1]
 print(array)
+print(f'{sum}')
